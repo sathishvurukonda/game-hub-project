211c4b69567da7552b476df0aea38f8d8a1d6c24
Sathish V <sathish.v@tallysolutions.com>
2025-03-02 00:43:25 +0530

    changes in app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack } from "@chakra-ui/react";
+import { Grid, GridItem, HStack, useDisclosure } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
 import GenreList from "./components/GenreList";
@@ -8,6 +8,7 @@ import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
 import GameHeading from "./components/GameHeading";
+import GenreDrawer from "./components/GenreDrawer";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -18,52 +19,67 @@ export interface GameQuery {
 
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   return (
-    <Grid
-      templateAreas={{
-        base: `"nav" "main"`,
-        lg: `"nav nav" "aside main"`,
-      }}
-      gridTemplateColumns={{
-        base: "1fr",
-        lg: "250px 1fr",
-      }}
-      gap={2}
-    >
-      <GridItem area="nav" p={4}>
-        <NavBar
-          onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
-        />
-      </GridItem>
-      <GridItem
-        area="aside"
-        paddingX={5}
-        display={{ base: "none", lg: "block" }}
+    <>
+      <GenreDrawer
+        isOpen={isOpen}
+        onClose={onClose}
+        onSelectGenre={(genre) => {
+          setGameQuery({ ...gameQuery, genre });
+          onClose(); // Close drawer after selection
+        }}
+        selectedGenre={gameQuery.genre}
+      />
+      <Grid
+        templateAreas={{
+          base: `"nav" "main"`,
+          lg: `"nav nav" "aside main"`,
+        }}
+        gridTemplateColumns={{
+          base: "1fr",
+          lg: "190px 1fr",
+        }}
+        gap={2}
       >
-        <GenreList
-          onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
-          selectedGenre={gameQuery.genre}
-        />
-      </GridItem>
-      <GridItem area="main">
-        <GameHeading gameQuery={gameQuery} />
-        <HStack spacing={5} mb={4}>
-          <PlatformSelector
-            onSelectPlatform={(platform) =>
-              setGameQuery({ ...gameQuery, platform })
+        <GridItem area="nav" p={4}>
+          <NavBar
+            onSearch={(searchText) =>
+              setGameQuery({ ...gameQuery, searchText })
             }
-            selectedPlatform={gameQuery.platform}
+            onOpenGenres={onOpen}
           />
-          <SortSelector
-            onSelectSortOrder={(sortorder) =>
-              setGameQuery({ ...gameQuery, sortorder })
-            }
-            selectedSortOrder={gameQuery.sortorder}
+        </GridItem>
+        <GridItem
+          area="aside"
+          paddingX={5}
+          display={{ base: "block", lg: "block" }}
+        >
+          <GenreList
+            onSelectGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
+            selectedGenre={gameQuery.genre}
           />
-        </HStack>
-        <GameGrid gameQuery={gameQuery} />
-      </GridItem>
-    </Grid>
+        </GridItem>
+        <GridItem area="main">
+          <GameHeading gameQuery={gameQuery} />
+          <HStack spacing={5} mb={4} paddingX={4}>
+            <PlatformSelector
+              onSelectPlatform={(platform) =>
+                setGameQuery({ ...gameQuery, platform })
+              }
+              selectedPlatform={gameQuery.platform}
+            />
+            <SortSelector
+              onSelectSortOrder={(sortorder) =>
+                setGameQuery({ ...gameQuery, sortorder })
+              }
+              selectedSortOrder={gameQuery.sortorder}
+            />
+          </HStack>
+          <GameGrid gameQuery={gameQuery} />
+        </GridItem>
+      </Grid>
+    </>
   );
 }
 
